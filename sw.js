386591b970a964a9dcab2b8a0c6201be21d7007c
Jonesy180 <andyjones180@yahoo.co.uk>
2026-09-20 19:33:41 +0100

    Add WWE 2K17 data and enable game

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='wtu-v0.10.0';
-const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','assets/icon.svg','data/2k18.json','data/2k19.json','data/2k20.json','data/2k22.json','data/2k23.json','data/2k24.json','data/2k25.json','data/2k26.json'];
+const CACHE='wtu-v0.11.0';
+const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','assets/icon.svg','data/2k17.json','data/2k18.json','data/2k19.json','data/2k20.json','data/2k22.json','data/2k23.json','data/2k24.json','data/2k25.json','data/2k26.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
